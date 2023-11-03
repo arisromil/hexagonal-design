1be85927b485d811bbce7676995d6fde87c6f0a3
arisromil <arisromil@yahoo.com>
2023-11-03 08:59:25 -0600

    Update CoreRouter.java

--- topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/entity/CoreRouter.java
+++ topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/entity/CoreRouter.java
@@ -1,19 +1,55 @@
 package dev.arisromil.topologyinventory.domain.entity;
 
+import dev.arisromil.topologyinventory.domain.specification.EmptyRouterSpec;
+import dev.arisromil.topologyinventory.domain.specification.EmptySwitchSpec;
 import dev.arisromil.topologyinventory.domain.specification.SameCountrySpec;
 import dev.arisromil.topologyinventory.domain.specification.SameIpSpec;
 import dev.arisromil.topologyinventory.domain.vo.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.Map;
 
 @Getter
 @ToString
 public final class CoreRouter extends Router {
-    public CoreRouter(Id id, Vendor vendor, Model model, IP ip, Location location, RouterType routerType) {
+
+    @Getter
+    private final Map<Id, Router> routers;
+
+    @Builder
+    public CoreRouter(Id id, Vendor vendor, Model model, IP ip, Location location, RouterType routerType, Map<Id, Router> routers) {
         super(id, vendor, model, ip, location, routerType);
+        this.routers = routers;
     }
 
+    public Router addRouter(Router anyRouter) {
+        var sameCountryRouterSpec = new SameCountrySpec(this);
+        var sameIpSpec = new SameIpSpec(this);
 
+        sameCountryRouterSpec.check(anyRouter);
+        sameIpSpec.check(anyRouter);
+
+        return this.routers.put(anyRouter.id, anyRouter);
+    }
+
+    public Router removeRouter(Router anyRouter) {
+        var emptyRoutersSpec = new EmptyRouterSpec();
+        var emptySwitchSpec = new EmptySwitchSpec();
+
+        switch (anyRouter.routerType) {
+            case CORE -> {
+                var coreRouter = (CoreRouter)anyRouter;
+                emptyRoutersSpec.check(coreRouter);
+            }
+            case EDGE -> {
+                var edgeRouter = (EdgeRouter)anyRouter;
+                emptySwitchSpec.check(edgeRouter);
+            }
+        }
+        return this.routers.remove(anyRouter.id);
+    }
 
 
 }
