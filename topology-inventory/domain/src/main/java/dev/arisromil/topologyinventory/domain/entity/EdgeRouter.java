841e8849f35878c3d1b8392de0bcbd7673f3ecdd
arisromil <arisromil@yahoo.com>
2023-11-03 08:59:29 -0600

    Update EdgeRouter.java

--- topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/entity/EdgeRouter.java
+++ topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/entity/EdgeRouter.java
@@ -1,5 +1,8 @@
 package dev.arisromil.topologyinventory.domain.entity;
 
+import dev.arisromil.topologyinventory.domain.specification.EmptyNetworkSpec;
+import dev.arisromil.topologyinventory.domain.specification.SameCountrySpec;
+import dev.arisromil.topologyinventory.domain.specification.SameIpSpec;
 import dev.arisromil.topologyinventory.domain.vo.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,5 +22,21 @@ public final class EdgeRouter extends Router {
         this.switches = switches;
     }
 
+    public void addSwitch(Switch anySwitch) {
+        var sameCountryRouterSpec = new SameCountrySpec(this);
+        var sameIpSpec = new SameIpSpec(this);
+
+        sameCountryRouterSpec.check(anySwitch);
+        sameIpSpec.check(anySwitch);
+
+        this.switches.put(anySwitch.id,anySwitch);
+    }
+
+    public Switch removeSwitch(Switch anySwitch) {
+        var emptyNetworkSpec = new EmptyNetworkSpec();
+        emptyNetworkSpec.check(anySwitch);
+
+        return this.switches.remove(anySwitch.id);
+    }
 
 }
