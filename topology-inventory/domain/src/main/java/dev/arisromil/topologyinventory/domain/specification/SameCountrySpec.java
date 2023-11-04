6cae31f5db17dbce8bdece8b5d7ab621841bb247
arisromil <arisromil@yahoo.com>
2023-11-04 11:50:56 -0600

    Update SameCountrySpec.java

--- topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/specification/SameCountrySpec.java
+++ topology-inventory/domain/src/main/java/dev/arisromil/topologyinventory/domain/specification/SameCountrySpec.java
@@ -1,8 +1,8 @@
 package dev.arisromil.topologyinventory.domain.specification;
 
-import dev.arisromil.topologyinventory.domain.entity.CoreRouter;
+import dev.arisromil.topologyinventory.domain.entity.EdgeRouter;
 
 public final class SameCountrySpec extends AbstractSpecification {
-    public SameCountrySpec(CoreRouter coreRouter) {
+    public SameCountrySpec(EdgeRouter coreRouter) {
     }
 }
