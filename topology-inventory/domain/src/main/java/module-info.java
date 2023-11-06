a3cce34eba782732cf59518f53733e970fb8395f
arisromil <arisromil@yahoo.com>
2023-11-06 00:03:25 -0700

    Update module-info.java

--- topology-inventory/domain/src/main/java/module-info.java
+++ topology-inventory/domain/src/main/java/module-info.java
@@ -1,3 +1,7 @@
 module domain {
+    exports dev.arisromil.topologyinventory.domain.entity;
+    exports dev.arisromil.topologyinventory.domain.service;
+    exports dev.arisromil.topologyinventory.domain.specification;
+    exports dev.arisromil.topologyinventory.domain.vo;
     requires static lombok;
 }
