d829c23f8dcf674139dfc1ea8af967a3ce0d466f
arisromil <arisromil@yahoo.com>
2023-11-08 10:36:13 -0700

    Defining the use case interface for switch management

---
+++ topology-inventory/application/src/main/java/dev/arisromil/topologyinventory/application/usecases/SwitchManagementUseCase.java
@@ -0,0 +1,17 @@
+package dev.arisromil.topologyinventory.application.usecases;
+
+import dev.arisromil.topologyinventory.domain.entity.EdgeRouter;
+import dev.arisromil.topologyinventory.domain.entity.Switch;
+import dev.arisromil.topologyinventory.domain.vo.*;
+
+public interface SwitchManagementUseCase {
+    Switch createSwitch(
+            Vendor vendor,
+            Model model,
+            IP ip,
+            Location location,
+            SwitchType switchType
+    );
+    EdgeRouter addSwitchToEdgeRouter(Switch networkSwitch, EdgeRouter edgeRouter);
+    EdgeRouter removeSwitchFromEdgeRouter(Switch networkSwitch, EdgeRouter edgeRouter);
+}
