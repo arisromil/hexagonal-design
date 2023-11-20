55f41c1eaffc2a24c05253583a6554d66f745b8f
arisromil <arisromil@yahoo.com>
2023-11-20 00:08:59 -0700

    Update module-info.java

--- topology-inventory/application/src/main/java/module-info.java
+++ topology-inventory/application/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module application {
     exports dev.arisromil.topologyinventory.application.ports.output;
+    exports dev.arisromil.topologyinventory.application.ports.input;
+    exports dev.arisromil.topologyinventory.application.usecases;
     requires domain;
     requires static lombok;
 }
